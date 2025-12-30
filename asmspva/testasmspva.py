7b9058c7d2595c5737cbd0c552b1081155c1ced3
vitaly <optinsoft@gmail.com>
2025-12-30 10:39:02 +0500

    test updated

--- asmspva/testasmspva.py
+++ asmspva/testasmspva.py
@@ -20,17 +20,16 @@ async def testAsyncSmsPva(apiKey: str):
 
     await testApi('getBalance', asmspva.getBalance('opt4'))
     await testApi('getUserInfo:', asmspva.getUserInfo('opt4'))
-    await testApi('getCountNew:', asmspva.getCountNew('opt4','RU'))
-    await testApi('getServicePrice', asmspva.getServicePrice('opt4','RU'))
-    number = await testApi('getNumber', asmspva.getNumber('opt4','RU'))
+    await testApi('getCountNew:', asmspva.getCountNew('opt4','US'))
+    await testApi('getServicePrice', asmspva.getServicePrice('opt4','US'))
+    number = await testApi('getNumber', asmspva.getNumber('opt4','US'))
     if number:
-        print(number)    
-        await testApi('getSMS', asmspva.getSMS('opt4','RU',number['id']))
+        await testApi('getSMS', asmspva.getSMS('opt4','US',number['id']))
         await testApi('getClearSMS', asmspva.getClearSMS('opt4',number['id']))
         await testApi('getProverka', asmspva.getProverka('opt4',number['number']))
         await testApi('balanceSIM', asmspva.balanceSIM('opt4',number['id']))    
-        await testApi('denial', asmspva.denial('opt4','RU',number['id']))
-        await testApi('ban', asmspva.denial('opt4','RU',number['id']))
+        await testApi('denial', asmspva.denial('opt4','US',number['id']))
+        await testApi('ban', asmspva.denial('opt4','US',number['id']))
     await testApi('get2FA', asmspva.get2FA('1234567890'))
 
     print('--- asmspva test completed ---')
